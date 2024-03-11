980a292e33d57f19d32aec37f1e4105fefca3cb8
Valentina Otočan <valentinaotocan@gmail.com>
2024-03-11 22:11:38 +0100

    Remove Skeleton and add spinner

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,32 +1,14 @@
-import { useState, useEffect } from "react";
 import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
 import useGames from "../hooks/useGames";
+import Spinner from "./Spinner";
 
 function Game() {
   const { data, isLoading, error } = useGames();
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 896);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsLargeScreen(window.innerWidth >= 896);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <>
       {error && <>Error 404</>}
-      {isLoading ? (
-        <>
-          {isLargeScreen
-            ? [1, 2, 3, 4, 5, 6].map((n) => <GameCardSkeleton key={n} />)
-            : [1, 2, 3].map((n) => <GameCardSkeleton key={n} />)}
-        </>
-      ) : (
+      {isLoading ? (<Spinner />) : (
         <>
           {data.map((game) => (
             <GameCard key={game.id} game={game} />
